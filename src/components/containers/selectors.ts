37a1cc20178ca74377f58f40104456439203bf41
rushi3828 <64717498+rushi3828@users.noreply.github.com>
2024-12-31 21:16:20 +0530

    Revert "crud-redux-store initial commit"

--- src/components/containers/selectors.ts
+++
@@ -1,9 +0,0 @@
-import { IStoreState } from "../../types";
-    
-export const isListSelector = (
-    (State: IStoreState) => State?.data?.dataManagement?.user?.userData
-);
-  
-export const isLoginSelector = (
-    (State: IStoreState) => State?.data?.dataManagement?.user?.loginData
-  );
--- src/store/ui/reducer.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import { userReducer } from "./login/slices";
-import { IDataManagementState } from "../types";
-
-export const IDataReducer = combineReducers<IDataManagementState>({
-    user:userReducer
-})
